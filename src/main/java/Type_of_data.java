e3094d09a6d69a13f9599b526f5177d58e111f2f
murtazintagir <tr.murtazin@yandex.ru>
2021-08-29 12:09:15 +0300

    Типы данных

---
+++ src/main/java/Type_of_data.java
@@ -0,0 +1,16 @@
+public class Type_of_data {
+    public static void main(String[] args){
+        byte num1 = 100; // 8 бит -128..127
+        short num2 = 131; //16 бит -32678..32767
+        int num3 = 141; //32 бит -2147483648..2147483647
+        long num4 = 123; //64 бит -9223372036854775808..9223372036854775807
+
+        float num5 = 189.6f; //32 0.123456 6-7
+        double num6 = 256.895; //64 15
+
+        char sym = '\u2122'; //16 бит
+
+        boolean isTrue = false; //1 бит
+
+    }
+}
